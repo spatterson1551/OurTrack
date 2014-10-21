223e24479b91bc7199415cdd508d2048e9a4c4c9
spatterson1551 <pattersonsw1@gcc.edu>
2014-10-21 13:26:44 -0400

    Added Like and Sort ajax capabilities
    Sorting drop downs now make calls to ajax to retrieve new items, and
    replies and tracks can all send ajax requests to like a post.

---
+++ js/changeSortMethod.js
@@ -0,0 +1,23 @@
+$(document).ready( function() {
+
+	$("#sortDropDown").change( function(event) {
+
+		var method = $("#sortDropDown option:selected").val();
+
+		var data = {
+			sortBy: method,
+		}
+
+		$.ajax({
+			type: 'POST',
+			url: 'some php script in the future', 
+			data: data,
+			success: function(data) { //event wont work until back end in place, then it will return replies in correct ordering
+				//output the returned replies
+			}
+		})
+	});
+
+
+
+});
